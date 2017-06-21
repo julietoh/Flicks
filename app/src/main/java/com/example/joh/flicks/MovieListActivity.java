bba7013ff0d8013bff96dd3709140c325c0636b7
Juliet Oh <joh@fb.com>
2017-06-21 16:21:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/joh/flicks/MovieListActivity.java
@@ -0,0 +1,90 @@
+package com.example.joh.flicks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieListActivity extends AppCompatActivity {
+    // constants
+    // the base URL for the API
+    public final static String API_BASE_URL = "https://api.themoviedb.org/3";
+    // the parameter name for the API key
+    public final static String API_KEY_PARAM = "api_key";
+    // tag for logging from this activity
+    public final static String TAG = "MovieListActivity";
+
+    // instance fields
+    AsyncHttpClient client;
+    // the base url for loading images
+    String imageBaseUrl;
+    // the poster size to use when fetching images, part of url
+    String posterSize;
+    // the list of currently playing movies
+    ArrayList<Movie> movies
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+
+        //initizlie the client
+        client = new AsyncHttpClient();
+        // get the configuration on appcreation
+        getConfiguration();
+
+    }
+
+    // get the configuration from the API
+    private void getConfiguration() {
+        // create the url
+        String url = API_BASE_URL + "/configuration";
+        // set the request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); // API key, always required
+        // execute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONObject images = response.getJSONObject("images");
+                    // get the image base url
+                    imageBaseUrl = images.getString("secure_base_url");
+                    // get the poster size
+                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+                    // use the option at index 3 or w342 as a fallback
+                    posterSize = posterSizeOptions.optString(3, "w342");
+                } catch (JSONException e) {
+                    logError("Failed parsing configuration", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed getting configuration", throwable, true);
+            }
+        });
+    }
+
+    // hangle errors, log and alert user
+    private void logError(String message, Throwable error, boolean alertUser) {
+        // always log the error
+        Log.e(TAG, message, error);
+        // alert the user to avoid silent errors
+        if (alertUser) {
+            // show a long toast with the error message
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+    }
+}
