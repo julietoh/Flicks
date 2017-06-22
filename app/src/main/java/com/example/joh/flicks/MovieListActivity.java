c9039088bf21217380c7a0b0334841af23f59b1c
Juliet Oh <joh@fb.com>
2017-06-22 16:16:00 -0700

    Required story

--- app/src/main/java/com/example/joh/flicks/MovieListActivity.java
+++ app/src/main/java/com/example/joh/flicks/MovieListActivity.java
@@ -2,9 +2,12 @@ package com.example.joh.flicks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.joh.flicks.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -12,6 +15,8 @@ import com.loopj.android.http.RequestParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -31,18 +36,68 @@ public class MovieListActivity extends AppCompatActivity {
     // the poster size to use when fetching images, part of url
     String posterSize;
     // the list of currently playing movies
-    ArrayList<Movie> movies
+    ArrayList<Movie> movies;
+    // the recycler view
+    RecyclerView rvMovies;
+    // the adapter wired to the recycler view
+    MovieAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
 
-        //initizlie the client
+        //initialize the client
         client = new AsyncHttpClient();
+        //initialize the list of movies
+        movies = new ArrayList<>();
         // get the configuration on appcreation
         getConfiguration();
+        // initialize the adapter - movies array cannot be reinitialized after this point
+        adapter = new MovieAdapter(movies);
 
+        // resolve the recycler view and connect a layour manager and the adapter
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+        rvMovies.setAdapter(adapter);
+
+        // get the configuration on app creation
+        getConfiguration();
+
+
+    }
+
+    // get the list of currently playing movies from the API
+    private void getNowPlaying() {
+        // create the url
+        String url = API_BASE_URL + "/movie/now_playing";
+        // set the request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); // API key, always required
+        // execute a GET request expecting a JSON object response
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // load the results into movies list
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    // iterate through result set and create Movie objects
+                    for (int i = 0; i < results.length(); i++) {
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                        // notify adapter that a row was added
+                        adapter.notifyItemInserted(movies.size() - 1);
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+                } catch (JSONException e) {
+                    logError("Failed to parse now playing movies", e, true);
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get data from now_playing endpoint", throwable, true);
+            }
+        });
     }
 
     // get the configuration from the API
@@ -65,6 +120,9 @@ public class MovieListActivity extends AppCompatActivity {
                     JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
                     // use the option at index 3 or w342 as a fallback
                     posterSize = posterSizeOptions.optString(3, "w342");
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and poster size %s", imageBaseUrl, posterSize));
+                    // get the now playing movie list
+                    getNowPlaying();
                 } catch (JSONException e) {
                     logError("Failed parsing configuration", e, true);
                 }
